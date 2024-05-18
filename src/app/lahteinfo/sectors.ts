4ba68556d8fe280630ef2fac3617148bcfd65e8b
kvartiil <138615442+kvartiil@users.noreply.github.com>
2024-05-18 21:06:15 +0300

    Initial commit

---
+++ src/app/lahteinfo/sectors.ts
@@ -0,0 +1,20 @@
+export const sektorid = [
+
+{
+  "sektorName": "Ehitus_1",
+  "sektorNo": '32'
+  },  
+{
+  "sektorName": "Ehitus_2",
+  "sektorNo": '33'
+  },
+{
+  "sektorName": "Ehitus_3",
+  "sektorNo": '34'
+  },  
+{
+  "sektorName": 'Mootorsõidukite hulgi ja jaemüük', 
+  "sektorNo": '35'
+}
+
+]
